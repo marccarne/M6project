8a44f3c68d35d3c41daaaed7171b86e1dfb54a82
anespereira <alejandro.nespereira@transport.alstom.com>
2017-03-07 14:35:08 +0100

    apply_H is now MATLAB compatible

--- lab1/apply_H.m
+++ lab1/apply_H.m
@@ -1,16 +1,19 @@
 function [rectifiedImage] = apply_H (I, H)
 
+rows = size(I,1);
+columns = size(I,2);
+
 Hinv = inv(H);
-corners = [1, 1,       columns(I) , columns(I) ;
-           1, rows(I) ,1          , rows(I)    ;
-           1, 1       ,1          , 1          ]
+corners = [1, 1,    columns , columns ;
+           1, rows, 1          , rows ;
+           1, 1,    1          , 1    ]
 
 rectifiedCorners = H * corners
 
 minCol = min(floor(min(rectifiedCorners(1,:))),1);
-maxCol = max(ceil (max(rectifiedCorners(1,:))),columns(I));
+maxCol = max(ceil (max(rectifiedCorners(1,:))),columns);
 minRow = min(floor(min(rectifiedCorners(2,:))),1);
-maxRow = max(ceil (max(rectifiedCorners(2,:))),rows(I));
+maxRow = max(ceil (max(rectifiedCorners(2,:))),rows);
 
 rectifiedWidth  = maxCol - minCol + 1
 rectifiedHeight = maxRow - minRow + 1
@@ -27,8 +30,8 @@ rectifiedCoordinates = [reshape(XI,1,numel(XI))                ;
 
 rectifiedCoordinatesAtOrigin = Hinv * rectifiedCoordinates;
 
-Xq = reshape(rectifiedCoordinatesAtOrigin(1,:),rows(XI),columns(YI));
-Yq = reshape(rectifiedCoordinatesAtOrigin(2,:),rows(XI),columns(YI));
+Xq = reshape(rectifiedCoordinatesAtOrigin(1,:),size(XI,1),size(YI,2));
+Yq = reshape(rectifiedCoordinatesAtOrigin(2,:),size(XI,1),size(YI,2));
 
 X = 1:1:columns(I);
 Y = 1:1:rows(I);
