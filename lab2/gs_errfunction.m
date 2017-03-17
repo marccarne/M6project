e238f144c961a126170dcc44a55ce96e31bde3c1
GonzaBCCC <gonzabenito@gmail.com>
2017-03-17 19:52:13 +0100

    advances on task 4
    no corrije bien los puntos, así que sigo debuggeando task 4

---
+++ lab2/gs_errfunction.m
@@ -0,0 +1,20 @@
+function [E] = gs_errfunction( P0, Xobs )
+
+H = reshape(P0(1:9),[3,3]);
+%Hinv = inv(H);
+
+%Convert points to homogeneous for applying H
+x = Xobs(1:(length(Xobs)/2));
+x = reshape(x,[2, (length(Xobs)/4)]);
+x_h = [x ; ones(1,size(x,2))];
+
+xp = Xobs(((length(Xobs)/2)+1):end);
+xp = reshape(xp,[2, (length(Xobs)/4)]);
+xp_h = [xp ; ones(1,size(xp,2))];
+
+x_est = H\xp_h;
+xp_est = H*x_h;
+
+E = [x - x_est(1:2,:); xp - xp_est(1:2,:)]';
+
+end
