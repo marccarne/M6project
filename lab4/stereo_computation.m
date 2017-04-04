8ae9ac955b54b99f08491f2e948e57b350d7af41
Alejandro-Alstom <alejandronespereira@gmail.com>
2017-04-04 14:13:17 +0200

    Added NCC

--- lab4/stereo_computation.m
+++ lab4/stereo_computation.m
@@ -1,6 +1,5 @@
-function disparity = stereo_computation(left_im, right_im, mindisp, maxdisp, wsize, cost)
-
-[h w]=size(left_im);
+function disparity = stereo_computation(l_img, r_img, mindisp, maxdisp, wsize, cost)
+[h w]=size(l_img);
 
 % The depth map has to start at 1 because MATLAB
 depth_map = zeros(h,w,1+maxdisp);
@@ -12,10 +11,10 @@ kernel = ones(wsize);
 % We compute the sumatory of the regions windowed at each pixel for later,
 % in case we use ncc 
 if strcmp(cost, 'ncc')
-    left = conv2(left_im,kernel,'same');
-    sigmaLeft = sqrt(conv2((double(left_im) - left).^2,kernel,'same'));
-    right = conv2(right_im,kernel,'same');
-    sigmaRight = sqrt(conv2((double(right_im) - right).^2,kernel,'same'));
+    left = conv2(double(l_img),double(kernel),'same')/wsize^2;
+    sigmaLeft = sqrt(conv2((double(l_img) - left).^2,kernel,'same'));
+    right = conv2(double(r_img),double(kernel),'same')/wsize^2;
+    sigmaRight = sqrt(conv2((double(r_img) - right).^2,kernel,'same'));
 end
 
 for d=mindisp:maxdisp
@@ -26,17 +25,31 @@ for d=mindisp:maxdisp
   if strcmp(cost, 'ssd')
     % if idx points to a pixel, idx - h*d points to the pixel d columns
     % before, which is the disparity measure.
-    disp(idx) = (left_im(idx) - right_im(idx - h*d)).^2;
+    disp(idx) = (l_img(idx) - r_img(idx - h*d)).^2;
     depth_map(:,:,d+1) = conv2(disp,kernel,'same');
   elseif strcmp(cost, 'ncc')
-    disp(idx) = ((double(left_im(idx)) - left(idx)) .* (double(right_im(idx - h*d)) - right(idx - h*d)))./(sigmaLeft(idx) .* sigmaRight(idx-h*d));
-    depth_map(:,:,d+1) = conv2(disp,kernel,'same');
+    disp(idx) = ((double(l_img(idx)) - left(idx)) .* (double(r_img(idx - h*d)) - right(idx - h*d)))./(sigmaLeft(idx) .* sigmaRight(idx-h*d));
+    % Correlation: high scores equal to matches. 
+    depth_map(:,:,d+1) = 1 -conv2(disp,kernel,'same');
   end    
 end
 
-% We get the disparity that got the minimal cost value
-[val,ind]=min(depth_map,[],3);
 
+% figure();
+% imagesc([depth_map(:,:,1),depth_map(:,:,2),depth_map(:,:,3),depth_map(:,:,4);
+%          depth_map(:,:,5),depth_map(:,:,6),depth_map(:,:,7),depth_map(:,:,8);
+%          depth_map(:,:,9),depth_map(:,:,10),depth_map(:,:,11),depth_map(:,:,12);
+%          depth_map(:,:,13),depth_map(:,:,14),depth_map(:,:,15),depth_map(:,:,16);]);
+% colormap(gray);
+%      
+% We get the disparity that got the minimal cost value
+% if strcmp(cost, 'ssd')
+%     [val,ind]=min(depth_map,[],3);
+% elseif strcmp(cost, 'ncc')
+%     [val,ind]=max(depth_map,[],3);
+% end
+     [val,ind]=min(depth_map,[],3);
 % We fix this because MATLAB is a horrible language and it needs constant
 % fixes to make sense
+
 disparity = ind-1;
